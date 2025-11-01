1132c66573bbd33927940730b2f7b57f8c7a8e39
AIIM00773 <ofrederick243@gmail.com>
2025-11-02 01:37:53 +0300

    .....

--- OneCart/core/urls.py
+++ OneCart/core/urls.py
@@ -6,11 +6,15 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-   path("",views.LndingPage.as_view()),
+   path('',views.OnecartHome.as_view()),
+   path("home/",views.OnecartHome.as_view()),
+   path("shop/",views.OnecartHome.as_view()),
+
    path("onecart-listings/", views.ListingsgPage.as_view()),
    path("onecart-ai/", views.OnecartaiPage.as_view()), 
    path("onecart-cart/",views.OneCartcartPage.as_view()), 
    path("oneCart-checkout/",views.OneCartcheckout.as_view()),
-   path("home/",views.OnecartHome.as_view()),
-   path("shop/",views.OnecartHome.as_view()),
+
+   path("about/",views.LndingPage.as_view()),
+
 ] 
