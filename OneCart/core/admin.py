fdaaf3109ce4c8f6615f6eff1c7461292987e1bb
AIIM00773 <ofrederick243@gmail.com>
2025-11-02 17:28:55 +0300

    .....

--- OneCart/core/admin.py
+++ OneCart/core/admin.py
@@ -1,9 +1,9 @@
 
 
 from django.contrib import admin
-from .models import (User, Marketplace, Product, Cart, CartItem,Order,OrderItem, AIPrompt)
+from .models import (User, Marketplace, Product, Cart, CartItem,Order,OrderItem, AIPrompt,GeneralMenuContent)
 
-modelsTuple = (User, Marketplace, Product, Cart, CartItem,Order,OrderItem, AIPrompt)
+modelsTuple = (User, Marketplace, Product, Cart, CartItem,Order,OrderItem, AIPrompt,GeneralMenuContent)
 
 for  model in modelsTuple:
     admin.site.register(model)
